036fe41254ef1cc2c234824a056bb070ed3e1a4a
Sherry He <54823224+sherry-hexichen@users.noreply.github.com>
2020-05-15 16:44:29 -0400

    Add python files for coding exercise 2 part 1 & 2

---
+++ codingExercise2Part1_replication.py
@@ -0,0 +1,200 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 14 22:26:55 2020
+
+@author: Sherry.He
+"""
+
+""" Coding Exercise 2. Part 1 - Replication - 1962 to 1991 """
+
+import pandas as pd
+import numpy as np
+import wrds
+conn = wrds.Connection(wrds_username='hexichen')
+
+# Sample Selection
+raw = conn.raw_sql("""
+                     select gvkey, fyear, OIADP, AT, ACT, CHE, LCT, DLC, TXP, DP
+                     from compa.funda WHERE
+                     exchg in (11,12) and (fyear<1991 and fyear>1960)
+                     """)
+
+# Variable Construction
+lag1 = raw.copy()
+lag1['fyear'] = lag1['fyear']+1
+lag1 = lag1.rename(columns={'at':'at_lag1', 'act':'act_lag1', 'che':'che_lag1', 'lct':'lct_lag1', 'dlc':'dlc_lag1', 'txp':'txp_lag1', 'dp':'dp_lag1'})
+
+data = pd.merge(raw, lag1, how='left', on=['gvkey','fyear'])
+data = data.dropna()
+
+# Earnings Component
+data['avgTotalAsset'] = (data['at']+data['at_lag1'])/2 # average total assets
+data['earnings'] = data['oiadp_x']/data['avgTotalAsset'] # earnings component
+# Accrual Component
+data['delta_ca'] = data['act']-data['act_lag1'] # change in current assets
+data['delta_cash'] = data['che']-data['che_lag1'] # change in cash
+data['delta_cl'] = data['lct']-data['lct_lag1'] # change in current liability
+data['delta_std'] = data['dlc']-data['dlc_lag1'] # change in debt included in current liabilities
+data['delta_tp'] = data['txp']-data['txp_lag1'] # change in income taxes payable
+ 
+data['acc'] = (data['delta_ca']-data['delta_cash']) - (data['delta_cl']-data['delta_std']-data['delta_tp']) - data['dp']
+data['accruals'] = data['acc']/data['avgTotalAsset'] # accrual component
+# Cashflow Component
+data['cashFlows'] = data['earnings'] - data['accruals'] # cash flow component
+
+fundaclean1 = data[['gvkey','fyear','earnings','accruals','cashFlows']].copy()
+
+# Portfolio Construction
+for year in fundaclean1.groupby(['fyear']):
+    fundaclean1['accrualDecile'] = pd.qcut(fundaclean1['accruals'], 10, labels=[1,2,3,4,5,6,7,8,9,10]) # rank on accruals
+    fundaclean1['earningDecile'] = pd.qcut(fundaclean1['earnings'], 10, labels=[1,2,3,4,5,6,7,8,9,10]) # rank on earnings
+    fundaclean1['cashFlowDecile'] = pd.qcut(fundaclean1['cashFlows'], 10, labels=[1,2,3,4,5,6,7,8,9,10]) # rank on cashFlows
+
+# Sum By Year
+variableSum = pd.DataFrame()
+variableSum['accrualsMean'] = fundaclean1.groupby(['accrualDecile'])['accruals'].mean()
+variableSum['accrualsMedian'] = fundaclean1.groupby(['accrualDecile'])['accruals'].median()
+variableSum['cashFlowsMean'] = fundaclean1.groupby(['accrualDecile'])['cashFlows'].mean()
+variableSum['cashFlowsMedian'] = fundaclean1.groupby(['accrualDecile'])['cashFlows'].median()
+variableSum['earningsMean'] = fundaclean1.groupby(['accrualDecile'])['earnings'].mean()
+variableSum['earningsMedian'] = fundaclean1.groupby(['accrualDecile'])['earnings'].median()
+table1 = variableSum.transpose() # Table 1 Panel A: Components of Earnings
+
+# Construct the Large Panel Data with 5 years lagging and 5 years leading 
+leadEarnings = fundaclean1[['gvkey','fyear']].copy()
+for i in range(1,6):
+    lead = fundaclean1[['gvkey','fyear','earnings']].copy()
+    lead['fyear'] = lead['fyear']-i
+    lead = lead.rename(columns={'earnings':'earnings_lead'+str(i)})
+    leadEarnings = pd.merge(leadEarnings, lead, how='left', on=['gvkey','fyear'])
+
+panelData = pd.merge(fundaclean1, leadEarnings, how='left', on=['gvkey','fyear'])
+
+lagEarnings = fundaclean1[['gvkey','fyear']].copy()
+for j in range(1,6):
+    lag = fundaclean1[['gvkey','fyear','earnings']].copy()
+    lag['fyear'] = lag['fyear']+j
+    lag = lag.rename(columns={'earnings':'earnings_lag'+str(j)})
+    lagEarnings = pd.merge(lagEarnings, lag, how='left', on=['gvkey','fyear'])
+    
+panelData = pd.merge(panelData, lagEarnings, how='left', on=['gvkey', 'fyear']) 
+
+# Regression
+
+# Pooled Regression
+import statsmodels.formula.api as sm
+#y = panelData['earnings_lead1']
+#x = panelData['earnings']
+table2_pooled = sm.ols(formula='earnings_lead1 ~ earnings', data=panelData).fit()
+table2_pooled.summary()
+
+table3_pooled = sm.ols(formula='earnings_lead1 ~ accruals + cashFlows', data=panelData).fit()
+table3_pooled.summary()
+
+# Collect SIC codes
+sic = conn.raw_sql("""select gvkey, sic from compa.company""")
+panelData = pd.merge(panelData, sic, on=['gvkey'])
+panelData['2_dig_SIC'] = panelData.sic.astype(str).str[:2].astype(int) # obtain the first 2 digits of the 4-digit SIC code
+
+eachIndus = panelData.groupby(['2_dig_SIC']).groups
+indusList = list(eachIndus.keys()) # list of all Industries
+
+#Industry Level Regression
+alpha0 = [] # intercept
+alpha1 = [] # earnings
+for i in indusList: 
+    indus = panelData.groupby(['2_dig_SIC']).get_group(i)
+    indusReg = sm.ols(formula='earnings_lead1 ~ earnings', data=indus).fit()
+    alpha0.append(indusReg.params[0])
+    alpha1.append(indusReg.params[1])
+
+table2_indus_alpha0 = pd.DataFrame(alpha0).describe()
+table2_indus_alpha1 = pd.DataFrame(alpha1).describe()
+
+gama0 = [] # intercept
+gama1 = [] # accruals
+gama2 = [] # cashFlows
+for i in indusList: 
+    indus = panelData.groupby(['2_dig_SIC']).get_group(i)
+    indusReg = sm.ols(formula='earnings_lead1 ~ accruals + cashFlows', data=indus).fit()
+    gama0.append(indusReg.params[0])
+    gama1.append(indusReg.params[1])
+    gama2.append(indusReg.params[2])
+    
+table3_indus_gama0 = pd.DataFrame(gama0).describe()
+table3_indus_gama1 = pd.DataFrame(gama1).describe()
+table3_indus_gama2 = pd.DataFrame(gama2).describe()
+
+# Figure: High and Low Accrual Portfolio
+decile1 = panelData.loc[panelData['accrualDecile'] == 1]
+decile1_earnings = decile1[['earnings_lag5','earnings_lag4','earnings_lag3','earnings_lag2', 'earnings_lag1',
+                   'earnings','earnings_lead1','earnings_lead2','earnings_lead3','earnings_lead4','earnings_lead5']]
+decile10 = panelData.loc[panelData['accrualDecile'] == 10]
+decile10_earnings = decile10[['earnings_lag5','earnings_lag4','earnings_lag3','earnings_lag2', 'earnings_lag1',
+                   'earnings','earnings_lead1','earnings_lead2','earnings_lead3','earnings_lead4','earnings_lead5']]
+
+
+extremeEarnings = pd.DataFrame()
+extremeEarnings['decile1'] = decile1_earnings.mean(axis=0)
+extremeEarnings['decile10'] = decile10_earnings.mean(axis=0)
+extremeEarningsData = extremeEarnings.rename(index={'earnings_lag5':'-5','earnings_lag4':'-4','earnings_lag3':'-3','earnings_lag2':'-2', 'earnings_lag1':'-1',
+                   'earnings':'0','earnings_lead1':'1','earnings_lead2':'2','earnings_lead3':'3','earnings_lead4':'4','earnings_lead5':'5'})
+
+import matplotlib.pyplot as plt
+plt.plot( 'decile1', data=extremeEarningsData, color='blue', linewidth=2)
+plt.plot( 'decile10', data=extremeEarningsData, color='orange', linewidth=2)
+plt.xlabel('Event Year')
+plt.ylabel('Mean Earnings')
+plt.title('Time Series Properties of Earnings with High and Low Accrual')
+plt.legend()
+plt.show() # second of Figure 1
+
+
+# Figure: High and Low Earning Portfolio
+decile1 = panelData.loc[panelData['earningDecile'] == 1]
+decile1_earnings = decile1[['earnings_lag5','earnings_lag4','earnings_lag3','earnings_lag2', 'earnings_lag1',
+                   'earnings','earnings_lead1','earnings_lead2','earnings_lead3','earnings_lead4','earnings_lead5']]
+decile10 = panelData.loc[panelData['earningDecile'] == 10]
+decile10_earnings = decile10[['earnings_lag5','earnings_lag4','earnings_lag3','earnings_lag2', 'earnings_lag1',
+                   'earnings','earnings_lead1','earnings_lead2','earnings_lead3','earnings_lead4','earnings_lead5']]
+
+
+extremeEarnings = pd.DataFrame()
+extremeEarnings['decile1'] = decile1_earnings.mean(axis=0)
+extremeEarnings['decile10'] = decile10_earnings.mean(axis=0)
+extremeEarningsData = extremeEarnings.rename(index={'earnings_lag5':'-5','earnings_lag4':'-4','earnings_lag3':'-3','earnings_lag2':'-2', 'earnings_lag1':'-1',
+                   'earnings':'0','earnings_lead1':'1','earnings_lead2':'2','earnings_lead3':'3','earnings_lead4':'4','earnings_lead5':'5'})
+
+import matplotlib.pyplot as plt
+plt.plot( 'decile1', data=extremeEarningsData, color='blue', linewidth=2)
+plt.plot( 'decile10', data=extremeEarningsData, color='orange', linewidth=2)
+plt.xlabel('Event Year')
+plt.ylabel('Mean Earnings')
+plt.title('Time Series Properties of Earnings with High and Low Earnings')
+plt.legend()
+plt.show() # first of Figure 1
+
+
+# Figure: High and Low cashFlow Portfolio
+decile1 = panelData.loc[panelData['cashFlowDecile'] == 1]
+decile1_earnings = decile1[['earnings_lag5','earnings_lag4','earnings_lag3','earnings_lag2', 'earnings_lag1',
+                   'earnings','earnings_lead1','earnings_lead2','earnings_lead3','earnings_lead4','earnings_lead5']]
+decile10 = panelData.loc[panelData['cashFlowDecile'] == 10]
+decile10_earnings = decile10[['earnings_lag5','earnings_lag4','earnings_lag3','earnings_lag2', 'earnings_lag1',
+                   'earnings','earnings_lead1','earnings_lead2','earnings_lead3','earnings_lead4','earnings_lead5']]
+
+
+extremeEarnings = pd.DataFrame()
+extremeEarnings['decile1'] = decile1_earnings.mean(axis=0)
+extremeEarnings['decile10'] = decile10_earnings.mean(axis=0)
+extremeEarningsData = extremeEarnings.rename(index={'earnings_lag5':'-5','earnings_lag4':'-4','earnings_lag3':'-3','earnings_lag2':'-2', 'earnings_lag1':'-1',
+                   'earnings':'0','earnings_lead1':'1','earnings_lead2':'2','earnings_lead3':'3','earnings_lead4':'4','earnings_lead5':'5'})
+
+import matplotlib.pyplot as plt
+plt.plot( 'decile1', data=extremeEarningsData, color='blue', linewidth=2)
+plt.plot( 'decile10', data=extremeEarningsData, color='orange', linewidth=2)
+plt.xlabel('Event Year')
+plt.ylabel('Mean Earnings')
+plt.title('Time Series Properties of Earnings with High and Low Cash Flows')
+plt.legend()
+plt.show() # third of Figure 1
---
+++ codingExercise2Part2_extension.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 23 19:23:02 2020
+
+@author: Sherry.He
+"""
+
+"""Coding Exercise 2. Part 2 - Extension - 2010 to 2018"""
+
+import pandas as pd
+import numpy as np
+import wrds
+conn = wrds.Connection(wrds_username='hexichen')
+
+raw = conn.raw_sql("""
+                     select gvkey, fyear, fyr, datadate, OIADP, AT, ACT, CHE, LCT, DLC, TXP, DP
+                     from compa.funda WHERE
+                     exchg in (11,12) and (fyear<2019 and fyear>2008)
+                     """)
+
+# Fundamental data from Compustat
+lag1 = raw.copy()
+lag1['fyear'] = lag1['fyear']+1
+lag1 = lag1.rename(columns={'at':'at_lag1', 'act':'act_lag1', 'che':'che_lag1', 'lct':'lct_lag1', 'dlc':'dlc_lag1', 'txp':'txp_lag1', 'dp':'dp_lag1'})
+
+data = pd.merge(raw, lag1, how='left', on=['gvkey','fyear'])
+
+data = data.dropna()
+
+data['avgTotalAsset'] = (data['at']+data['at_lag1'])/2 # average total assets
+data['earnings'] = data['oiadp_x']/data['avgTotalAsset'] # earnings component
+
+data['delta_ca'] = data['act']-data['act_lag1'] # change in current assets
+data['delta_cash'] = data['che']-data['che_lag1'] # change in cash
+data['delta_cl'] = data['lct']-data['lct_lag1'] # change in current liability
+data['delta_std'] = data['dlc']-data['dlc_lag1'] # change in debt included in current liabilities
+data['delta_tp'] = data['txp']-data['txp_lag1'] # change in income taxes payable
+ 
+data['acc'] = (data['delta_ca']-data['delta_cash']) - (data['delta_cl']-data['delta_std']-data['delta_tp']) - data['dp']
+data['accruals'] = data['acc']/data['avgTotalAsset'] # accrual component
+data['cashFlows'] = data['earnings'] - data['accruals'] # cash flow component
+
+fundaclean1 = data[['gvkey','fyear','fyr_x','datadate_x','earnings','accruals','cashFlows','avgTotalAsset']].copy()
+fundaclean1 = fundaclean1.rename(columns={'fyr_x':'fyr', 'datadate_x':'datadate'})
+
+fundaclean2 = pd.DataFrame()
+yearlist=fundaclean1.fyear.unique().astype(int).tolist()
+yearlist.sort()
+for year in yearlist:
+              fundacleantmp=fundaclean1[fundaclean1['fyear']==year]
+              fundacleantmp['accrualDecile'] = pd.qcut(fundacleantmp['accruals'], 10, labels=[1,2,3,4,5,6,7,8,9,10])
+              fundaclean2=fundaclean2.append(fundacleantmp)
+
+# Stock return data from CRSP
+crsp_m = conn.raw_sql("""
+                      select a.permno, a.permco, a.date, b.shrcd, b.exchcd,
+                      a.ret, a.retx, a.shrout, a.prc
+                      from crsp.msf as a
+                      left join crsp.msenames as b
+                      on a.permno=b.permno
+                      and b.namedt<=a.date
+                      and a.date<=b.nameendt
+                      where a.date between '01/01/2010' and '12/31/2019'
+                      and b.exchcd between 1 and 2
+                      and b.shrcd between 10 and 11
+                      """) 
+
+# delisting return(dlret) and delisting date(dlstdt)
+dlret = conn.raw_sql("""
+                     select permno, dlret, dlstdt 
+                     from crsp.msedelist
+                     """)
+dlret['dlstdt']=pd.to_datetime(dlret['dlstdt']) # convert to datetime format
+dlret['jdate']=dlret['dlstdt']+pd.offsets.MonthEnd(0) # from dlstdt to jdate: moving the date to end of each month
+
+# add jdate, delisting-adjusted return, market value
+# line up date to be end of month
+crsp_m['date']=pd.to_datetime(crsp_m['date']) 
+crsp_m['jdate']=crsp_m['date']+pd.offsets.MonthEnd(0)   
+# adjust delisting return
+crsp = pd.merge(crsp_m, dlret, how='left',on=['permno','jdate'])
+crsp['dlret']=crsp['dlret'].fillna(0)
+crsp['ret']=crsp['ret'].fillna(0)
+crsp['retadj']=(1+crsp['ret'])*(1+crsp['dlret'])-1
+# market value
+crsp['me']=crsp['prc'].abs()*crsp['shrout'] 
+
+# add size-decile return
+ermport2 = conn.raw_sql("""
+                      select *
+                      from crsp.ermport2 as a
+                      where a.date between '01/01/2010' and '12/31/2019'
+                      """) 
+ermport2['date']=pd.to_datetime(ermport2['date'])
+# merge CRSP with ermport based on permno and date
+crsp = pd.merge(crsp, ermport2, how='left', on=['permno','date'])
+
+crsp['retadjsize'] =crsp['retadj']-crsp['decret']
+
+crsp_final = crsp[['permno','permco','date','shrcd','exchcd','ret_x','retx','jdate','retadj','me','decret','retadjsize']].copy() 
+crsp_final = crsp_final.rename(columns={'ret_x':'ret'}) # final CRSP dataset: permno - month - return, each company has a return for each month
+
+# use CCM to merge CRSP with Compustat
+ccm=conn.raw_sql("""
+                  select gvkey, lpermno as permno, linktype, linkprim, 
+                  linkdt, linkenddt
+                  from crsp.ccmxpf_linktable
+                  where substr(linktype,1,1)='L'
+                  and (linkprim ='C' or linkprim='P')
+                  """)
+ccm['linkdt']=pd.to_datetime(ccm['linkdt'])
+ccm['linkenddt']=pd.to_datetime(ccm['linkenddt'])
+ccm['linkenddt']=ccm['linkenddt'].fillna(pd.to_datetime('today')) # if linkenddt is missing then set to today's date
+
+# Portfolio Construction
+ccm1 = pd.merge(fundaclean2, ccm, how='left', on=['gvkey'])
+# Portfolio starting date: 4 months after fiscal year end. Return starts 5 months after fiscal year end.
+ccm1['jdate0']=ccm1['datadate']+pd.offsets.MonthEnd(5)
+ccm2=ccm1[(ccm1['jdate0']>=ccm1['linkdt'])&(ccm1['jdate0']<=ccm1['linkenddt'])] # jdate between linkdt and linkenddt
+# Portfolio ending date: holding 12 months after starting date
+ccm2['jdate1']=ccm2['jdate0']+pd.offsets.MonthEnd(11)
+
+# merge ccm2 with CRSP by permno and date
+crspcomp = pd.merge(ccm2, crsp_final, how='left', on=['permno'])
+crspcomp=crspcomp[(crspcomp['jdate']>=crspcomp['jdate0'])&(crspcomp['jdate']<=crspcomp['jdate1'])]
+crspcomp=crspcomp[['gvkey','datadate','fyear','accrualDecile','permno','jdate0','jdate1','retadj','retadjsize']].copy()
+
+# Buy and Hold Return
+crspcomp['1+retadj']=crspcomp['retadj']+1
+crspcomp['1+retadjsize']=crspcomp['retadjsize']+1
+# Group by gvkey, fyear: 
+
+crspcomp['cumretrawyr1']=crspcomp.groupby(['gvkey','datadate'])['1+retadj'].cumprod()-1 
+crspcomp['cumretsizeadjyr1']=crspcomp.groupby(['gvkey','datadate'])['1+retadjsize'].cumprod()-1 
+
+# Final Dataset: gvkey---fyear---annualReturn
+crspcomp_final = crspcomp.groupby(['gvkey','datadate']).nth(11).reset_index()
+crspcomp_final = crspcomp_final[['gvkey','datadate','fyear','accrualDecile','cumretrawyr1','cumretsizeadjyr1']].copy()
+
+# Table 6
+# group by accrualDecile and calculate mean returns
+portret = crspcomp_final.groupby('accrualDecile')['cumretrawyr1','cumretsizeadjyr1'].mean()
+
+# group by year and calculate hedge returns
+calHedgeReturn = crspcomp_final.groupby(['fyear','accrualDecile'])['cumretrawyr1','cumretsizeadjyr1'].mean().reset_index()
+calHedgeReturn = calHedgeReturn.dropna()
+years = calHedgeReturn['fyear'].unique()
+hedgeReturn = pd.DataFrame(columns=['fyear','hedgerawret','hedgesizeadjret'])
+
+for y in list(years):
+    peryear = calHedgeReturn[calHedgeReturn['fyear'] == y]
+    h1 = float(peryear[peryear['accrualDecile']==1]['cumretrawyr1'])-float(peryear[peryear['accrualDecile']==10]['cumretrawyr1'])
+    h2 = float(peryear[peryear['accrualDecile']==1]['cumretsizeadjyr1'])-float(peryear[peryear['accrualDecile']==10]['cumretsizeadjyr1'])
+    write = pd.Series([y, h1, h2], index=hedgeReturn.columns)
+    hedgeReturn = hedgeReturn.append(write, ignore_index=True)
+
+# Figure 2
+import matplotlib.pyplot as plt
+pos = list(range(len(hedgeReturn['fyear'])))
+width = 0.38
+
+year = [int(round(x)) for x in list(years)]
+fit, ax = plt.subplots(figsize=(10,5))
+
+plt.bar(pos, hedgeReturn['hedgerawret'], width, alpha=0.5, color='blue')
+plt.bar([p + width for p in pos], 
+        hedgeReturn['hedgesizeadjret'],
+        width, 
+        alpha=0.5, 
+        color='orange') 
+plt.xticks(pos, year, rotation='vertical')
+ax.set_xlabel('Year')
+ax.set_ylabel('Hedge Return')
+ax.set_title('Annual Hedge Returns')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
